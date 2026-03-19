acecec5d2ae8973628537a43c8740581956b4333
Ankit Sharma <ankitsharma2615@gmail.com>
2026-03-19 21:58:15 +0530

    add node realtime status using inngest realtime

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -4,11 +4,19 @@ import prisma from "@/lib/prisma";
 import { topologicalSort } from "./utils";
 import { NodeType } from "@/generated/prisma/enums";
 import { getExecutor } from "@/features/executions/lib/executor-registry";
+import { httpRequestChannel } from "./channels/http-request";
+import { manualTriggerChannel } from "./channels/manual-trigger";
 
 export const executeWorkflow = inngest.createFunction(
-  { id: "excecute-workflow" },
-  { event: "workflows/execute.workflow" },
-  async ({ event, step }) => {
+  {
+    id: "excecute-workflow",
+    retries: 0, // TODO: REMOVE IN PRODUCTION
+  },
+  {
+    event: "workflows/execute.workflow",
+    channels: [httpRequestChannel(), manualTriggerChannel()],
+  },
+  async ({ event, step, publish }) => {
     const workflowId = event.data.workflowId;
 
     if (!workflowId) {
@@ -40,6 +48,7 @@ export const executeWorkflow = inngest.createFunction(
         nodeId: node.id,
         context,
         step,
+        publish,
       });
     }
     return { workflowId, result: context };
